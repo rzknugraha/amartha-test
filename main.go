3918fc762eee1e6255745438420944f7ec746b02
Rizky Nugraha <rizkynugraha@Rizkys-MacBook-Pro.local>
2021-04-11 22:53:27 +0700

    feat: make endpoint get shorten

--- main.go
+++ main.go
@@ -54,8 +54,11 @@ type ShortenResp struct {
 }
 
 type DataURL struct {
-	Shortcode string
-	URL       string
+	Shortcode    string
+	URL          string
+	CreatedAt    string
+	LastSeenDate string
+	Count        int
 }
 
 var DataByShorten map[string][]*DataURL
@@ -75,6 +78,7 @@ func main() {
 
 	r := mux.NewRouter()
 	r.HandleFunc("/shorten", short).Methods("POST")
+	r.HandleFunc("/shortcode/{shorten}", getShort).Methods("GET")
 
 	validate = validator.New()
 	validate.RegisterTagNameFunc(func(fld reflect.StructField) string {
@@ -136,14 +140,14 @@ func short(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if p.Shortcode == "" {
+	if p.Shortcode == "" || len(p.Shortcode) < 0 {
 		shorten = randSeq(6)
 	} else {
 		shorten = p.Shortcode
 	}
 	fmt.Println("shorten")
 	fmt.Println(shorten)
-	getData := getDataByShorten(shorten)
+	getData, _ := getDataByShorten(shorten)
 
 	if getData {
 		Response(w, http.StatusConflict, &ShortenResp{
@@ -153,8 +157,11 @@ func short(w http.ResponseWriter, r *http.Request) {
 		return
 
 	}
-	dTemp.Shortcode = p.Shortcode
+	fmt.Println("p.Shortcode")
+	fmt.Println(p.Shortcode)
+	dTemp.Shortcode = shorten
 	dTemp.URL = p.URL
+	dTemp.CreatedAt = time.Now().Format("2006-01-02T15:04:05+00:00")
 
 	store(dTemp)
 
@@ -166,23 +173,57 @@ func short(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func store(v DataURL) {
+func getShort(w http.ResponseWriter, r *http.Request) {
+	shorten := mux.Vars(r)["shorten"]
 
+	if shorten == "" {
+		Response(w, http.StatusNotFound, &ShortenResp{
+			Message: "Not Found",
+		})
+		return
+	}
+
+	checkShorten, data := getDataByShorten(shorten)
+	fmt.Println("checkShorten")
+	fmt.Println(checkShorten)
+	if checkShorten {
+		http.Redirect(w, r, data.URL, http.StatusFound)
+		return
+	}
+	Response(w, http.StatusNotFound, &ShortenResp{})
+	return
+
+}
+
+func store(v DataURL) {
+	fmt.Println(v.Shortcode)
+	fmt.Println("v.Shortcode")
+	fmt.Println(v)
 	DataByShorten[v.Shortcode] = append(DataByShorten[v.Shortcode], &v)
 }
 
-func getDataByShorten(s string) bool {
+func getDataByShorten(s string) (state bool, data DataURL) {
 	fmt.Println("s")
 	fmt.Println(s)
+	fmt.Println(DataByShorten)
+	fmt.Println(len(DataByShorten[s]))
 	if len(DataByShorten[s]) > 0 {
+
 		for _, val := range DataByShorten[s] {
 			fmt.Println("val")
 			fmt.Println(val)
-			if val.Shortcode != "" {
-				return true
+			data = DataURL{
+				Shortcode:    val.Shortcode,
+				URL:          val.URL,
+				CreatedAt:    val.CreatedAt,
+				LastSeenDate: val.LastSeenDate,
+				Count:        val.Count,
 			}
+			return true, data
+
 		}
+
 	}
 
-	return false
+	return false, data
 }
